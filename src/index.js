80eecde2c092859c553f2fe74e65466ab250b99d
Daniel Whiffing <danielwhiffing@gmail.com>
2022-01-21 18:19:02 -0500

    Makes game responsive

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser'
 import GameScene from './scenes/Game'
+import UpgradeScene from './scenes/Upgrade'
 import MenuScene from './scenes/Menu'
 import BootScene from './scenes/Boot'
 
@@ -8,14 +9,10 @@ new Phaser.Game({
   backgroundColor: '#c0c0c0',
   parent: 'phaser',
   width: 500,
-  height: 350,
-  // scale: { mode: Phaser.Scale.RESIZE },
-  physics: {
-    default: 'arcade',
-    arcade: { debug: false },
-  },
+  height: 270,
+  scale: { mode: Phaser.Scale.FIT, autoCenter: true },
+  physics: { default: 'arcade', arcade: { debug: false } },
   zoom: 2,
-  scene: [BootScene, MenuScene, GameScene],
+  scene: [BootScene, MenuScene, GameScene, UpgradeScene],
   pixelArt: true,
-  // roundPixels: true,
 })
